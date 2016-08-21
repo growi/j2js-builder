4351dd6feac2e440308dd15affdfe313009752f7
growi <growi@ankertaugasse.de>
2016-08-21 02:28:18 +0200

    refactoring JSONEntityFactory to singelton, fixed scope

--- SimpleJSON/src/org/j2js/json/adapter/JSONEntityFactory.java
+++ SimpleJSON/src/org/j2js/json/adapter/JSONEntityFactory.java
@@ -8,25 +8,46 @@ import org.j2js.json.values.*;
 
 public class JSONEntityFactory {
 	
-	private final static JSONNullValue NULL_VALUE = JSONNullValue.getInstance();
+	protected static JSONEntityFactory m_instance;
+	protected final static JSONNullValue NULL_VALUE = JSONNullValue.getInstance();
 	
-	public static JSONEntity getAdapter( String value ){
+	protected JSONEntityFactory(){
+	}
+	
+	public static JSONEntityFactory getInstance(){
+		if( m_instance == null ){
+			m_instance = new JSONEntityFactory();	
+		}
+		return m_instance;
+	}
+	
+	public JSONEntity getAdapter( Object o ){
+		if( o == null ) return NULL_VALUE;
+		if( o instanceof String ) return getAdapter( (String)o );
+		if( o instanceof Number ) return getAdapter( (Number)o );
+		if( o instanceof Boolean )return getAdapter( (Boolean)o );
+		if( o instanceof Date ) return getAdapter( (Date)o );
+		if( o instanceof List<?> )return getAdapter( (List<?>)o );
+		return null;
+	}
+	
+	public JSONEntity getAdapter( String value ){
 		return value == null ? NULL_VALUE : new JSONStringValue( value );
 	}
 	
-	public static JSONEntity getAdapter( Number value ){
+	public JSONEntity getAdapter( Number value ){
 		return value == null ? NULL_VALUE : new JSONNumberValue( value );
 	}
 	
-	public static JSONEntity getAdapter( Boolean value ){
+	public JSONEntity getAdapter( Boolean value ){
 		return value == null ? NULL_VALUE : new JSONBooleanValue( value );
 	}
 	
-	public static JSONEntity getAdapter( Date value ){
+	public JSONEntity getAdapter( Date value ){
 		return value == null ? NULL_VALUE : new JSONDateTimeValue( value );
 	}
 
-	public static JSONEntity getAdapter( List<?> value ){
+	public JSONEntity getAdapter( List<?> value ){
 		return value == null ? NULL_VALUE : new JSONListAdapter( (List<?>) value );
 	}
 }
